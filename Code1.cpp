0d4141d11a33faa429f62f90c28df336e4fd8714
Mayrita16 <mayraavarg52@hotmail.com>
2025-02-01 20:30:40 -0600

    Created our first C++ for coding 1
    learning to use github, good commit messages and descriptions matter for finding bugs

---
+++ Code1.cpp
@@ -0,0 +1,35 @@
+// Code1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+
+
+
+
+#include <iostream>                 // this allows us to send text to and from the console.     
+using namespace std;                // this allows (tabs are great) us to NOT type "std::" all the GD time.
+int main()                          // this is where our program starts.                          
+{
+    std::cout << "Hello World!\n";      // this line sends text to the console.
+
+    std::cout << "Hi Mayra and Lan!\n";
+    // variables are nicknames for data. 
+    // variable type is string, variable name is "name", variable value is "Jinx".      
+    string name = "Player";   //from arcane 
+    // Variable type is integer, variable name is health, variable value is 45
+    int health = 45;
+
+    cout << "My name is " << name << ".\n";
+    cout << "I have " << health << " health" ".\n";
+    
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
